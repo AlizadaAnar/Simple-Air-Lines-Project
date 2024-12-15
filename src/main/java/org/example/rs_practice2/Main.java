073630ffce444f6d7e47dda4a32e406e1848a559
anaralizada <anar_alizade@internet.ru>
2024-12-16 01:04:49 +0400

    added

---
+++ src/main/java/org/example/rs_practice2/Main.java
@@ -0,0 +1,32 @@
+package org.example.rs_practice2;
+
+class JobExecutor implements Runnable {
+    private String jobLabel;
+
+    public JobExecutor(String jobLabel) {
+        this.jobLabel = jobLabel;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 5; i > 0; i--) {
+            System.out.println(jobLabel + " - Time left: " + i);
+            try {
+                Thread.sleep(1000); //checked exception, waits 1 second
+            } catch (InterruptedException e) {
+                System.out.println(jobLabel + " was stopped.");
+            }
+        }
+        System.out.println(jobLabel + " - Job finished!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Thread task1 = new Thread(new JobExecutor("Operation 1"));
+        Thread task2 = new Thread(new JobExecutor("Operation 2"));
+
+        task1.start();
+        task2.start();
+    }
+}
